74f4376c07107cb1675bf11bac7290009771704e
jwhogg <jhogg2@sheffield.ac.uk>
2026-07-08 15:40:20 +0100

    model eval

---
+++ code/model_evaluation.py
@@ -0,0 +1,13 @@
+"""
+This Code uses the gold layer dataset, and evaluates a number of models for detecting defects
+"""
+
+
+class Model:
+    def __init__(self):
+        return None
+
+
+# need to have a generic model class that is capable of taking data and producing predictions
+# whatever models that are to be tested will use this generic class
+# will have a seperate evaluation function
